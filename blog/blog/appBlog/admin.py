1d8856a7e40066f3bcc6d7d36b358035569661f3
mattiviera <matiiviera38@gmail.com>
2025-01-23 16:10:41 -0300

    registro los admins, luego de haber creado los modelos

--- blog/blog/appBlog/admin.py
+++ blog/blog/appBlog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here. una vez hechos los modelos, debemos registrarlos en el admin
+
+from django.contrib import admin
+from .models import Post, Category, Tag, Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'is_published')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('is_published', 'categories', 'tags')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at', 'is_approved')
+    search_fields = ('user__username', 'post__title', 'content')
+    list_filter = ('is_approved',)
